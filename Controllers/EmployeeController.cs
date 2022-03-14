3bce91185d9bdff5a3e9698b2078c029d3fb70f0
Chandan <Chandan@DESKTOP-7G0P5Q8>
2022-03-14 12:10:43 -0700

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext db;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            db = context;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult GetData()
+        {
+            var abc = db.Employees.ToList();
+            return View(abc);
+        }
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+            db.Employees.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("GetData");
+        }
+        public IActionResult Delete(int id)
+        {
+            var dlt = db.Employees.Where(m => m.id == id).FirstOrDefault();
+            db.Employees.Remove(dlt);
+            db.SaveChanges();
+            return RedirectToAction("GetData");
+        }
+        public IActionResult Edit(int id)
+        {
+            var ab = db.Employees.Where(m => m.id == id).FirstOrDefault();
+            return View(ab);
+        }
+        public IActionResult Details(int id)
+        {
+            var ab = db.Employees.Where(m => m.id == id).FirstOrDefault();
+            return View(ab);
+        }
+
+    }
+}
---
+++ Migrations/20220314175125_m.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication5.Migrations
+{
+    public partial class m : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    age = table.Column<int>(nullable: false),
+                    mobile = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
